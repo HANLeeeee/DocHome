6d10e5835949496ab523d855a0c25ec62ba6af14
HANLE <hanleeeee@naver.com>
2022-09-10 22:28:34 +0900

    스크롤뷰추가중

--- DocHome/Detail/SearchDetailView.swift
+++ DocHome/Detail/SearchDetailView.swift
@@ -11,6 +11,16 @@ import UIKit
 
 class SearchDetailView: UIView {
     
+    lazy var scrollView = { () -> UIScrollView in
+        let view = UIScrollView()
+        return view
+    }()
+    
+    lazy var contentView = { () -> UIView in
+        let view = UIView()
+        return view
+    }()
+
     lazy var mapView = { () -> UIView in
         let view = UIView()
         view.backgroundColor = .red
@@ -31,8 +41,23 @@ class SearchDetailView: UIView {
         return view
     }()
     
-    lazy var alabel = { () -> UILabel in
+    lazy var titleLabel = { () -> UILabel in
         let label = UILabel()
+        label.textColor = UIColor(named: "COLOR_PURPLE")
+        label.text = "어머니 프랑시스 북간도에 한 위에 나는 지나가는 이국 듯합니다. 이름과 하나에 어머님, 거외다. 너무나 불러 쉬이 봄이 다하지 까닭입니다. 하나 아이들의 않은 시와 둘 듯합니다. 까닭이요, 사랑과 아침이 까닭이요, 아이들의 하나의 라이너 까닭입니다. 흙으로 많은 아침이 슬퍼하는 하나에 밤이 하나의 덮어 옥 까닭입니다."
+        label.font = .systemFont(ofSize: 22, weight: .bold)
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    lazy var locationLabel = { () -> UILabel in
+        let label = UILabel()
+        label.textColor = .darkGray
+        label.text = "어머니 프랑시스 북간도에 한 위에 나는 지나가는 이국 듯합니다. 이름과 하나에 어머님, 거외다. 너무나 불러 쉬이 봄이 다하지 까닭입니다. 하나 아이들의 않은 시와 둘 듯합니다. 까닭이요, 사랑과 아침이 까닭이요, 아이들의 하나의 라이너 까닭입니다. 흙으로 많은 아침이 슬퍼하는 하나에 밤이 하나의 덮어 옥 까닭입니다."
+        label.font = .systemFont(ofSize: 22, weight: .bold)
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
         return label
     }()
     
@@ -54,20 +79,48 @@ class SearchDetailView: UIView {
     
     //MARK: - func
     func addViews() {
-        self.addSubview(mapView)
+        self.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        
+        contentView.addSubview(mapView)
         mapView.addSubview(mapLocationView)
+        
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(locationLabel)
     }
     
     func makeConstraints() {
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalTo(self.safeAreaLayoutGuide)
+        }
+        
+        contentView.snp.makeConstraints { make in
+            make.edges.equalTo(scrollView)
+            make.height.greaterThanOrEqualTo(self.snp.height).priority(.low)
+            make.width.equalTo(scrollView.snp.width)
+        }
+        
         mapView.snp.makeConstraints { make in
-            make.top.equalTo(self.safeAreaLayoutGuide)
-            make.centerX.equalToSuperview()
+            make.top.equalTo(self.contentView)
             make.height.equalTo(Constants.Device.width)
             make.width.equalTo(Constants.Device.width)
         }
         
         mapLocationView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
+            make.edges.equalTo(self.mapView)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(mapView.snp.bottom).offset(30)
+            make.left.equalTo(20)
+            make.right.equalTo(-20)
+            make.bottom.equalTo(20)
+        }
+        
+        locationLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(30)
+            make.left.equalTo(20)
+            make.right.equalTo(-20)
         }
     }
 }
