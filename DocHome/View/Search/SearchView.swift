c17d4689d72b9f18ad8ce8295c705f8144c32b3b
HANLE <hanleeeee@naver.com>
2023-03-10 03:01:38 +0900

    makeConstraints 호출위치 수정

--- DocHome/View/Search/SearchView.swift
+++ DocHome/View/Search/SearchView.swift
@@ -61,16 +61,12 @@ class SearchView: UIView {
         super.init(frame: frame)
         self.backgroundColor = .white
         addViews()
+        makeConstraints()
     }
         
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    override func layoutSubviews() {
-        makeConstraints()
-    }
-    
     
     //MARK: - func
     func addViews() {
